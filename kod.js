8541f8bb5d00a4b60cf513b6b6c3e3b8b534a512
PITRv1 <145228064+PITRv1@users.noreply.github.com>
2026-02-25 11:06:42 +0100

    asd
    asd

--- kod.js
+++ kod.js
@@ -1,8 +1,22 @@
 function nagyitKep(melyikKep) {
+    let imgDisplay = document.getElementById("nagykep")
+    imgDisplay.src = melyikKep.src
 }
 
 function visszaKep() {
+    let imgDisplay = document.getElementById("nagykep")
+    imgDisplay.src = "ures.jpg"
+
 }
 
 function kivalaszt(melyikTermek) {
+    console.log("a")
+
+    let cartDisplay = document.getElementById("kosar")
+    let newText = document.createElement("p")
+    
+    let db = prompt("Hány darabot szeretne?")
+    newText.innerText = db + " db " + melyikTermek.innerText
+    cartDisplay.appendChild(newText)
+
 }
